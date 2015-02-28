88924253d8c0e49780b2f57dc19b552b008ed548
karina678 <karina@terra-karina.de>
2015-02-28 16:58:38 +0100

    moved todo list to readme file

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # terra-onepage
 Private experimantal project
+
+## What I would like to build and test:
++ accessibility
++ shiny css stuff
++ blinking javascript without jquery and the like
++ usability with javascript turned off/not available in user's browser
++ fonts/icon fonts
++ svg
++ drag & drop
++ audio
++ video
++ performance
++ maybe some canvas??
++ box-shadow über eck (siehe ecp)
--- index.php
+++ index.php
@@ -16,36 +16,6 @@ include_once('templates/head.phtml');
 <?php include_once('templates/header.phtml'); ?>
 
 <main>
-    <section id="what-is-this-place">
-        <h2>What is this Place?</h2>
-
-        <ul>
-            <li>
-                place to train my dev skills (frontend), like:
-                <ul>
-                    <li>accessibility</li>
-                    <li>shiny css stuff</li>
-                    <li>blinking javascript without jquery and the like</li>
-                    <li>usability with javascript turned off/not available in user's browser</li>
-                    <li>fonts/icon fonts</li>
-                    <li>svg</li>
-                    <li>audio</li>
-                    <li>video</li>
-                    <li>performance</li>
-                    <li>maybe some canvas??</li>
-                    <li>box-shadow über eck (siehe ecp)</li>
-                </ul>
-            </li>
-            <li>show those skills off</li>
-        </ul>
-
-        <strong>What this place is not:</strong>
-        <ul>
-            <li>some features may not work in all browsers - given that this is an experimentation site i try to mainly use current/new features. in some cases of course the point is to make features work in as many browsers as possible (or available for my testing).</li>
-            <li>tutorial (i recommend rummaging in the code)</li>
-        </ul>
-    </section>
-
     <section id="accessible-modal-window">
         <h2>Accessible modal window</h2>
 
@@ -57,20 +27,6 @@ include_once('templates/head.phtml');
                 http://substantial.com/blog/2014/07/22/how-i-audit-a-website-for-accessibility/
             </cite>
         </footer>
-    </section>
-
-    <section id="drag-and-drop">
-        <h2>A little bit of Drag & Drop</h2>
-    </section>
-
-    <section id="inspiration">
-        <h2>Inspiration</h2>
-
-        <ul>
-            <li>
-                <a href="https://www.mozilla.org/de/firefox/os/">FirefoxOS website</a>
-            </li>
-        </ul>
     </section>
 </main>
 
