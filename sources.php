092d9717787d34fef02904de305cc9529cc45586
karina678 <karina@terra-karina.de>
2017-09-16 23:41:55 +0200

    added links to sources

--- sources.php
+++ sources.php
@@ -24,6 +24,8 @@ include('templates/header.phtml');
         <p>Further sources:</p>
         <ul>
             <li>Code syntax highlighting: <a href="http://prismjs.com/">Prism.js</a>  by <a href="http://lea.verou.me/">Lea Verou</a> and <a href="https://github.com/LeaVerou/prism/contributors">others</a></li>
+            <li>List of predefined easing functions (e.g. for transitions) using B&eacute;zier curves: <a href="http://easings.net">Easings Cheat Sheet</a> by <a href="https://twitter.com/andreysitnik">Andrey Sitnik</a>.</li>
+            <li><a href="http://cubic-bezier.com">B&eacute;zier curve generator</a>, again by Lea Verou.</li>
         </ul>
         </section>
     </main>
